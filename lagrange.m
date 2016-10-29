aa6932911e1530ed3e38d24044d22a81ee727bee
Gigahawk <Gigahawk@users.noreply.github.com>
2016-10-29 02:48:36 -0700

    Create lagrange.m

---
+++ lagrange.m
@@ -0,0 +1,38 @@
+
+function f = lagrange(r,n,a,subX)
+    % I quite honestly have no idea what this does 
+    syms x;
+    
+    % Completely unnecessary but whatevs
+    n = n+1;
+    
+    % Finds the negative of lagrange formula(optimization function only
+    % finds minimums so it needs to be flipped)
+    init = -((diff(r,x,n))*subs((x-a)^(n),x,subX))/factorial(n);
+    
+    % Somethingsomething data types matlab sure is confusing
+    g = matlabFunction(init);
+    
+    % Finds x value "subX" and "a" uninclusive such that g is at its
+    % minimum (I have no idea how to make fminbind search inclusive so
+    % whatever)
+    minimumX = fminbnd(g,subX,a);
+    
+    % Inverts initial calculation for final solution
+    init = -init;
+    
+    % ghetto solution to make it inclusive
+    test1 = subs(init, x, minimumX);
+    test2 = subs(init, x, subX);
+    test3 = subs(init, x, a);
+    
+    if test1 < test2
+        output = test2;
+    elseif test1 < test3
+        output = test3;
+    else
+        output = test1;
+    end
+    
+    f=output
+end
